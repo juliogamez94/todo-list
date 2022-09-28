76a6e48a9e2b42ac4d09dda616ef61d2d3615035
juliogamez94 <juliogamezb@gmail.com>
2022-09-27 19:58:42 -0600

    Add todoList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Todo App</div>;
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    setTodoList([...todoList, newTask]);
+  };
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input type="text" value={newTask} onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todoList &&
+          todoList.map((task) => {
+            return <h1>{task}</h1>;
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
