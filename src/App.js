8f8de172472708373209796bf4e7cbc5a1bc83bc
juliogamez94 <juliogamezb@gmail.com>
2022-09-27 22:36:20 -0600

    DeleteTask added and id added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,18 @@ function App() {
   };
 
   const addTask = () => {
-    setTodoList([...todoList, newTask]);
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    const newTodoList = todoList.filter((task) => {
+      return task.id !== id;
+    });
+    setTodoList(newTodoList);
   };
   return (
     <div className="App">
@@ -21,7 +32,12 @@ function App() {
       <div className="list">
         {todoList &&
           todoList.map((task) => {
-            return <h1>{task}</h1>;
+            return (
+              <div>
+                <h1>{task.taskName}</h1>
+                <button onClick={() => deleteTask(task.id)}>X</button>
+              </div>
+            );
           })}
       </div>
     </div>
